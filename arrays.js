b4463524ec5b7dfad860c47ef0887ea5275721df
ANGELO SPAMPINATO <spampinatodesign@gmail.com>
2018-12-01 20:54:42 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray() {
+	var array = [1, 2, 3, 4];
+	[0, ...array];
+	array
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
